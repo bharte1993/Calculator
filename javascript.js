529b4d876f51aef03ba65b86cd0f4df02d55877a
bhartendra <bhartendra.singh55@gmail.com>
2018-02-21 13:03:29 +0530

    updated code

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
  var decimal=false;
  var lastOperationCall=false;
  function clearDisplay()
-{ 
+{
    document.getElementById("display").value ="";
    decimal=false;
    lastOperationCall=false;
@@ -18,20 +18,17 @@ function insertDecimal(dec) {
       decimal=true;
      }
 function setOperator(op){
+ lastOperationCall=false;
    var display = document.getElementById("display");
    l=display.value.charAt(display.value.length-1);
    const set = new Set(["+", ".", "-", "*", "/"]);
-     if((op==='-'&&l === "*")||(op=='-'&&l === "/")){
-        display.value += op;
-         return; 
-     }
-      if(set.has(l)){
+    if(set.has(l)){
           return;
      }
   display.value += op;
       decimal=false;
-      
-     
+
+
     }
 function getlastOperation(displayValue){
   var lastoperation=displayValue.charAt(displayValue.length-1);
@@ -41,41 +38,64 @@ function getlastOperation(displayValue){
             return lastoperation;
        }
 }
-     
+
 }
 
-function calculate() { 
+function calculate() {
 
-	try{     
+	try{
                  display=document.getElementById("display");
                  checkOperations=display.value.includes("+")||display.value.includes("-")||display.value.includes("*")||display.value.includes("/");
-                   if(checkOperations&&lastOperationCall===false){ 
+                   if(checkOperations&&lastOperationCall===false){
                    lastOperation=getlastOperation(display.value);
                  }
-                
+
                   if(checkOperations&&lastOperationCall===false){
+                      console.log("dg");
+                     lastOperationCall=true;
                      display.value=eval(display.value);
                    }
-                  else{ 
-                         lastOperationCall=true;
+                  else{
+                         
                          a=display.value+lastOperation;
                          display.value=eval(a);
-                    } 
+                    }
                  }catch(err){
          document.getElementById("display").value="Syntex Error";
  }
 }
 
-function keyPress(e){
+function keydown(e){
 var key=(e.which) ? (e.which) : (e.keyCode) || (e.charCode);
+console.log(String.fromCharCode(key));
+if(key===37||key===39){
+ return true;
+}
 if(key===13){
   calculate();
 }
-if((key>64&&key<91)||key===186||key===188||key===190||key===191||key===192||key===220||key===222||key===219||key===221||key===187||key===189){ 
- 
-  return false;
- }
+if(key>95&&key<106){
+ return true;
+}
+if(String.fromCharCode(key)==='n'||String.fromCharCode(key)==='k'||String.fromCharCode(key)==='m'||String.fromCharCode(key)==='j'||String.fromCharCode(key)==='o'){
+if(String.fromCharCode(key)==='n'){
+   insertDecimal('.');
+  }
+if(String.fromCharCode(key)==='m'){
+   setOperator('-');
+  }
+if(String.fromCharCode(key)==='o'){
+   setOperator('/');
+  }
+if(String.fromCharCode(key)==='j'){
+   setOperator('*');
+  }
+if(String.fromCharCode(key)==='k'){
+   setOperator('+');
+  }
+}
+if(key>31&&(key<48||key>57)){
+   return false;
 }
 
-
-
+}
